3eaf10bc41ccbe7ddf16ece97c831fcdd538c5d5
Aman Prakash Jha <amanjha7151@gmail.com>
2021-04-24 02:50:04 +0530

    callibrating for android

--- main.py
+++ main.py
@@ -86,7 +86,7 @@ class PongGame(Widget):
     player2 = ObjectProperty(None)
 
     def serve_ball(self):
-        self.ball.velocity = Vector(4,0).rotate(randint(0,360))
+        self.ball.velocity = Vector(8,0).rotate(randint(0,360))
 
     def update(self, dt):
         #moving the ball by calling the move method 
@@ -109,9 +109,9 @@ class PongGame(Widget):
 
 
     def on_touch_move(self, touch):
-        if touch.x < self.width/ 1/4:
+        if touch.x < self.width/ 1/2:
             self.player1.center_y = touch.y
-        if touch.x > self.width * 3/4:
+        if touch.x > self.width * 1/2:
             self.player2.center_y = touch.y
 
 class PongApp(App):
